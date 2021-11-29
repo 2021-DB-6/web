d2162259250922582f88efb287fcd6820e440c6a
azxcv24 <azxcv24@gmail.com>
2021-11-29 03:32:21 +0000

    초기 세팅

--- php/checkout.php
+++ php/checkout.php
@@ -138,7 +138,7 @@
         </div>
     </section>
     <!-- Footer-->
-    <footer class="py-5 bg-dark fixed-bottom">
+    <footer class="py-5 bg-dark">
         <div class="container">
             <p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p>
         </div>
